daeeda54a361208abe79329438684712c327c83c
bcasas-upc <64041242+bcasas-upc@users.noreply.github.com>
2020-04-20 22:10:22 +0200

    Update geany.rb
    Updated download url. Last version of geany can be found at this new url.

--- Casks/geany.rb
+++ Casks/geany.rb
@@ -2,7 +2,7 @@ cask 'geany' do
   version '1.36'
   sha256 'cdd3feb6209e96efcc1302423f3f7d915274966d20969eed9fc6496aef53b034'
 
-  url "https://download.geany.org/geany-#{version}_osx.dmg"
+  url "https://download.geany.org/geany-#{version}_osx-2.dmg"
   appcast 'https://github.com/geany/geany/releases.atom'
   name 'Geany'
   homepage 'https://www.geany.org/'
